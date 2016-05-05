723cf23c5e2cdf458bb270843aa0ce8caff53c11
Haroun Hajem <haroun.hajem@hotmail.com>
2016-05-05 16:24:49 +0200

    recovery Begin

---
+++ Quiz_WPFVersion/HelperClass/QuestionTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using Quiz_WPFVersion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Quiz_WPFVersion.HelperClass
+{
+    class QuestionTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var question = item as Question;
+            FrameworkElement element = container as FrameworkElement;
+            if (question == null || element == null) return null;
+
+            DataTemplate template = element.FindResource(question.Type.ToString()) as DataTemplate;
+
+            return template;
+        }
+
+    }
+}
