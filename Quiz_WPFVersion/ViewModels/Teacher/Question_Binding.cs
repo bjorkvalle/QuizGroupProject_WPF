9056a98835412b35dc78a36952ca90f3d8512322
Haroun Hajem <haroun.hajem@hotmail.com>
2016-05-07 19:54:59 +0200

    Question_Binding

---
+++ Quiz_WPFVersion/ViewModels/Teacher/Question_Binding.cs
@@ -0,0 +1,14 @@
+﻿using Quiz_WPFVersion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_WPFVersion.ViewModels.Teacher
+{
+    class Question_Binding : Question
+    {
+        public int ScoreValue { get; set; }
+    }
+}
