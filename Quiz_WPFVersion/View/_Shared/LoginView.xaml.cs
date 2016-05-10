79f4cf05f807aff8ea7861fc52edf0ef083657d7
Callemist <calle.magnus.sandberg@gmail.com>
2016-05-10 10:58:06 +0200

    FIxing view model

--- Quiz_WPFVersion/View/_Shared/LoginView.xaml.cs
+++ Quiz_WPFVersion/View/_Shared/LoginView.xaml.cs
@@ -31,14 +31,14 @@ namespace Quiz_WPFVersion.View._Shared
         {
             InitializeComponent();
 
-            //User user = new User
-            //{
-            //    Name = "Test2",
-            //    Password = "123",
-            //    Type = UserType.Teacher
-            //};
+            User user = new User
+            {
+                Name = "Test2",
+                Password = "123",
+                Type = UserType.Admin
+            };
 
-            //Repository<User>.GetInstance().AddData(user);
+            Repository<User>.GetInstance().AddData(user);
 
 
         }
--- Quiz_WPFVersion/ViewModels/Statistics_ViewModel.cs
+++ Quiz_WPFVersion/ViewModels/Statistics_ViewModel.cs
@@ -21,118 +21,118 @@ namespace Quiz_WPFVersion.ViewModels
         public ObservableCollection<Education> _Education { get; private set; }
         public ObservableCollection<User> _Users { get; private set; }
 
-        public ObservableCollection<Statistics_Quiz> _Quiz { get; private set; }
+        public ObservableCollection<Quiz> _Quiz { get; private set; }
 
         private Statistics viewStatistics { get; set; }
 
         public Statistics_ViewModel()
         {
 
-            _Quiz = new ObservableCollection<Statistics_Quiz>
-            {
-                new Statistics_Quiz
-                {
-                    Title = "Prov1",
-                    AverageTime = GetAverageTime(CreateQuizzes()),
-                    QuizTaken = 14,  //Metod för att se hur många som är klara
-                    QuizNotTaken = 2, //Metod för att se hur många som INTE är klara
-                    GradeG = 13,  //Metod för att hämta alla godkända
-                    GradeF = 1   //Metod för att hämta alla underkända
-                },
-                new Statistics_Quiz
-                {
-                    Title = "Prov2",
-                    AverageTime = GetAverageTime(CreateQuizzes()),
-                    QuizTaken = 30,
-                    QuizNotTaken = 0,
-                    GradeG = 20,
-                    GradeF = 10
-                },
-                new Statistics_Quiz
-                {
-                    Title = "Prov3",
-                    AverageTime = GetAverageTime(CreateQuizzes()),
-                    QuizTaken = 37,
-                    QuizNotTaken = 2,
-                    GradeG = 33,
-                    GradeF = 5
+            //_Quiz = new ObservableCollection<Statistics_Quiz>
+            //{
+            //    new Statistics_Quiz
+            //    {
+            //        Title = "Prov1",
+            //        AverageTime = GetAverageTime(CreateQuizzes()),
+            //        QuizTaken = 14,  //Metod för att se hur många som är klara
+            //        QuizNotTaken = 2, //Metod för att se hur många som INTE är klara
+            //        GradeG = 13,  //Metod för att hämta alla godkända
+            //        GradeF = 1   //Metod för att hämta alla underkända
+            //    },
+            //    new Statistics_Quiz
+            //    {
+            //        Title = "Prov2",
+            //        AverageTime = GetAverageTime(CreateQuizzes()),
+            //        QuizTaken = 30,
+            //        QuizNotTaken = 0,
+            //        GradeG = 20,
+            //        GradeF = 10
+            //    },
+            //    new Statistics_Quiz
+            //    {
+            //        Title = "Prov3",
+            //        AverageTime = GetAverageTime(CreateQuizzes()),
+            //        QuizTaken = 37,
+            //        QuizNotTaken = 2,
+            //        GradeG = 33,
+            //        GradeF = 5
 
-                },
-                new Statistics_Quiz
-                {
-                    Title = "Prov4",
-                    AverageTime = GetAverageTime(CreateQuizzes()),
-                    QuizTaken = 32,
-                    QuizNotTaken = 5,
-                    GradeG = 2,
-                    GradeF = 30
-
-
-                },
-            };
+            //    },
+            //    new Statistics_Quiz
+            //    {
+            //        Title = "Prov4",
+            //        AverageTime = GetAverageTime(CreateQuizzes()),
+            //        QuizTaken = 32,
+            //        QuizNotTaken = 5,
+            //        GradeG = 2,
+            //        GradeF = 30
 
 
-            #region Edu, Course, User + Repository
+            //    },
+            //};
 
-            _Users = new ObservableCollection<User>
-            {
-                new User
-                {
-                    Name = "Ba",
-                },
-                new User
-                {
-                    Name = "Ka",
-                },
-                new User
-                {
-                    Name = "Raad",
-                },
-                new User
-                {
-                    Name = "Da",
-                },
-                new User
-                {
-                    Name = "Fz",
-                },
 
-            };
+            //#region Edu, Course, User + Repository
 
-            _Courses = new ObservableCollection<Course>()
-            {
-                new Course
-                {
-                    Name = "Logic",
+            //_Users = new ObservableCollection<User>
+            //{
+            //    new User
+            //    {
+            //        Name = "Ba",
+            //    },
+            //    new User
+            //    {
+            //        Name = "Ka",
+            //    },
+            //    new User
+            //    {
+            //        Name = "Raad",
+            //    },
+            //    new User
+            //    {
+            //        Name = "Da",
+            //    },
+            //    new User
+            //    {
+            //        Name = "Fz",
+            //    },
 
-                }, new Course
-                {
-                    Name = "Basic",
+            //};
 
-                },
-                 new Course
-                {
-                    Name = "Expert",
+            //_Courses = new ObservableCollection<Course>()
+            //{
+            //    new Course
+            //    {
+            //        Name = "Logic",
 
-                },
+            //    }, new Course
+            //    {
+            //        Name = "Basic",
 
-            };
-            _Education = new ObservableCollection<Education>()
-            {
-                new Education
-                {
-                    Name = "Java"
-                },
-                new Education
-                {
-                    Name = "C-Sharp"
-                },
-                 new Education
-                {
-                    Name = "Phyton"
-                },
+            //    },
+            //     new Course
+            //    {
+            //        Name = "Expert",
 
-            };
+            //    },
+
+            //};
+            //_Education = new ObservableCollection<Education>()
+            //{
+            //    new Education
+            //    {
+            //        Name = "Java"
+            //    },
+            //    new Education
+            //    {
+            //        Name = "C-Sharp"
+            //    },
+            //     new Education
+            //    {
+            //        Name = "Phyton"
+            //    },
+
+            //};
 
             //var quizzes = Repository<Quiz>.GetInstance().GetDataList().Where(quiz => quiz.UserId == ActiveUser.Id);
 
@@ -144,7 +144,9 @@ namespace Quiz_WPFVersion.ViewModels
             //education_temp.ForEach(u => _Education.Add(u));
 
             //Education = repo_Education.GetDataList();
-            #endregion
+            //#endregion
+
+            _Education = new ObservableCollection<Education>(Repository<Education>.GetInstance().GetDataList());
 
         }
 
@@ -176,7 +178,7 @@ namespace Quiz_WPFVersion.ViewModels
                       Title = "JavaQuiz",
                       StartDate = new DateTime(2016, 05, 07, 13, 00, 00),
                       EndDate = new DateTime(2016, 05, 07, rand.Next(13,17), rand.Next(00,59), 00),
-                      
+
                 },
                 new Quiz
                 {
@@ -256,16 +258,42 @@ namespace Quiz_WPFVersion.ViewModels
         {
             if (selEducation != null)
             {
+                _Users = new ObservableCollection<User>(Repository<User>.GetInstance().GetDataList().
+                     Where(user => user.Education_Id == selEducation.Id));
 
-            }else if (selCourse != null)
+                //_Quiz = new ObservableCollection<Quiz>(Repository<Quiz>.GetInstance().GetDataList().
+                //     Where(quiz => quiz.UserId ==));
+
+
+                //IList<Quiz> quizzes = Repository<Quiz>.GetInstance().GetDataList();
+
+                //List<Quiz> tmpQuizzes = new List<Quiz>();
+
+                //foreach (var quiz in quizzes)
+                //{
+                //    foreach (var user in _Users)
+                //    {
+                //        if (quiz.UserId == user.Id)
+                //            tmpQuizzes.Add(quiz);
+                //    }
+                //}
+
+
+            }
+            else if (selCourse != null)
             {
 
-            } else if(selUser != null)
+            }
+            else if (selUser != null)
             {
 
             }
 
             //sök med tre värden
+
+
+
+
         }
 
         public void SetSelCourse(object selCourse)
