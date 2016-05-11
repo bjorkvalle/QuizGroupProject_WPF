52c1df06bdca7d642f5a4c0a3d909418d32624bb
Haroun Hajem <haroun.hajem@hotmail.com>
2016-05-11 10:30:44 +0200

    New update

--- Quiz_WPFVersion/Migrations/201605110814306_Init.Designer.cs
+++ Quiz_WPFVersion/Migrations/201605110814306_Init.Designer.cs
@@ -7,13 +7,13 @@ namespace Quiz_WPFVersion.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Second : IMigrationMetadata
+    public sealed partial class Init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Second));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Init));
         
         string IMigrationMetadata.Id
         {
-            get { return "201605101447443_Second"; }
+            get { return "201605110814306_Init"; }
         }
         
         string IMigrationMetadata.Source
