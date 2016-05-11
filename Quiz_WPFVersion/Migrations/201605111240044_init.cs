6249e80789503b04ad0bc3030d35fdc9baf40e89
Callemist <calle.magnus.sandberg@gmail.com>
2016-05-11 14:44:57 +0200

    Migration files

--- Quiz_WPFVersion/Migrations/201605111240044_init.cs
+++ Quiz_WPFVersion/Migrations/201605111240044_init.cs
@@ -3,7 +3,7 @@ namespace Quiz_WPFVersion.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Init : DbMigration
+    public partial class init : DbMigration
     {
         public override void Up()
         {
